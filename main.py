22605bca408facb6387120fa050120ba05ac48fd
sent1nu11 <sent1nu11@gmail.com>
2021-08-12 07:25:13 -0600

    add scoreboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 # test
 
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 game_is_on = True
 while game_is_on:
@@ -28,6 +30,7 @@ while game_is_on:
   #Detect collision with food
   if snake.head.distance(food) < 15:
     food.refresh()
+    scoreboard.increase_score()
 
 
 screen.exitonclick()
