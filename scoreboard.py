cb5e95cf2dd50e568cc1f1ce0cd1183f5a76efd0
sent1nu11 <sent1nu11@gmail.com>
2021-08-28 22:29:08 -0600

    added read.py

--- scoreboard.py
+++ scoreboard.py
@@ -6,24 +6,28 @@ FONT = ("courier", 14, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.high_score = 0
         self.score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER")
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.clear()
+        self.update_scoreboard()
 
 
