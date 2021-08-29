61cc1c2a3514b72c639c113a5093463c29467845
sent1nu11 <sent1nu11@gmail.com>
2021-08-29 12:05:36 -0600

    add high score option to save permanently after program exists

--- read.py
+++
@@ -1,2 +0,0 @@
-with open('data.txt', 'w') as file:
-    
--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.high_score = 0
+        with open("data.txt") as file:
+    #    with open("/Users/rogerwu/Desktop/data.txt", mode="r") as file:
+            self.high_score = int(file.read())
         self.score = 0
         self.color("white")
         self.penup()
@@ -25,6 +28,9 @@ class Scoreboard(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
             self.high_score = self.score
         self.score = 0
         self.clear()
